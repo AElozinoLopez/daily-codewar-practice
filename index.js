837e6b2592b8392656a85dbc2106602b9bd60082
ElozinoLopez <asaijeelozinolopez@gmail.com>
2025-09-08 07:44:02 +0100

    Wrote the function to reverse string

--- index.js
+++ index.js
@@ -5,3 +5,14 @@
 // or more letters reversed (Just like the name of this Kata).
 // Strings passed in will consist of only letters and spaces. S
 // paces will be included only when more than one word is present.
+
+function spinWords(string) {
+  return string
+    .split(" ")
+    .map((word) =>
+      word.length >= 5 ? word.split(" ").reverse().join("") : word
+    )
+    .join("");
+}
+
+console.log(spinWords("This is the first practice"));
