8b58c340a4cd9ed6103583f5af20fbd6774f80d0
ElozinoLopez <asaijeelozinolopez@gmail.com>
2025-09-11 07:07:35 +0100

    Called the rowSumOddNumbers funciton for rows 2 to 5

--- index.js
+++ index.js
@@ -86,3 +86,7 @@ function rowSumOddNumbers(n) {
 }
 
 console.log(rowSumOddNumbers(1));
+console.log(rowSumOddNumbers(2));
+console.log(rowSumOddNumbers(3));
+console.log(rowSumOddNumbers(4));
+console.log(rowSumOddNumbers(5));
