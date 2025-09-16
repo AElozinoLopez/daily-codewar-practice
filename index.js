f9b9541dab95647bc3cb1e5d98faf5064ffcf2f9
ElozinoLopez <asaijeelozinolopez@gmail.com>
2025-09-16 08:36:51 +0100

    Added an example to practice 4 for clarity

--- index.js
+++ index.js
@@ -136,3 +136,16 @@ console.log(divisors(13));
 // Collect all numbers that divide n evenly (n % i === 0).
 
 // If no divisors are found, return the string "<n> is prime".
+
+//////////////////////////////////////////////////////////////////////
+
+// Practice 5
+// Implement a function that computes the difference between two lists.
+// The function should remove all occurrences of elements from the first
+// list (a) that are present in the second list (b). The order of elements
+// in the first list should be preserved in the result.
+
+// Examples
+// If a = [1, 2] and b = [1], the result should be [2].
+
+// If a = [1, 2, 2, 2, 3] and b = [2], the result should be [1, 3].
