9ce778b01eaff55994d688826211926bbd74f404
ElozinoLopez <asaijeelozinolopez@gmail.com>
2025-09-16 08:41:04 +0100

    Decleared an empty function called arrayDiff

--- index.js
+++ index.js
@@ -149,3 +149,6 @@ console.log(divisors(13));
 // If a = [1, 2] and b = [1], the result should be [2].
 
 // If a = [1, 2, 2, 2, 3] and b = [2], the result should be [1, 3].
+
+// Solution
+function arrayDiff(a, b) {}
