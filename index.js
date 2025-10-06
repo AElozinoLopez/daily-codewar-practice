c10dfc60c48df3c08c5f660da985b4fb3a0f4e6a
ElozinoLopez <asaijeelozinolopez@gmail.com>
2025-10-06 07:48:04 +0100

    Added challenge 7 and created the function to solve the challenge

--- index.js
+++ index.js
@@ -189,3 +189,19 @@ function isPrime(num) {
 }
 
 console.log(isPrime(4));
+
+////////////////////////////////////////////////////////////////////////////
+
+// Practice 7
+// Write a function that takes an integer as input, and returns the number of bits that are equal to one
+// in the binary representation of that number. You can guarantee that input is non-negative.
+
+// Example: The binary representation of 1234 is 10011010010, so the function should return 5 in this case
+
+// Solution
+function countBits(n) {
+  return n
+    .toString(2) // To convert the number to binary (base 2)
+    .split(" ") // To split binary to individaul bits
+    .filter((bit) => bit === "1").length; // Keep only '1's // To count how many wants there are
+}
